48f22275c87560f307514994e7888c912f79a1b5
Kamran Ramzan <kamranramzan098@gmail.com>
2016-07-27 04:20:39 +0500

    7/27/2016

--- app/src/main/java/abbottabad/comsats/campusapp/HomePageView.java
+++ app/src/main/java/abbottabad/comsats/campusapp/HomePageView.java
@@ -9,7 +9,10 @@ import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
@@ -20,6 +23,7 @@ import com.google.android.gms.common.GooglePlayServicesUtil;
 public class HomePageView extends AppCompatActivity {
 
     private static final String PREFERENCE_FILE_KEY = "abbottabad.comsats.campusapp";
+    private AlertDialog alertDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,33 +33,56 @@ public class HomePageView extends AppCompatActivity {
         new InitialPageController(this).execute();
         isPlayServicesAvailable();
 
-        SharedPreferences sharedPreferences = this.getSharedPreferences(PREFERENCE_FILE_KEY, Context.MODE_PRIVATE);
-        final String APPLICATION_STATUS = sharedPreferences.getString("APPLICATION_STATUS", "NULL");
-
         CardView cv_bloodBank = (CardView) findViewById(R.id.CV_bloodBank);
-        if (APPLICATION_STATUS.equals("STUDENT")){
-            if (cv_bloodBank != null) {
-                cv_bloodBank.setVisibility(View.VISIBLE);
-                cv_bloodBank.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        startActivity(new Intent(HomePageView.this, BloodBankView.class));
-                    }
-                });
-            }
-        }
-        else if (APPLICATION_STATUS.equals("BLOOD_BANK")){
-            if (cv_bloodBank != null) {
-                cv_bloodBank.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        startActivity(new Intent(HomePageView.this, BloodBankView.class));
-                    }
-                });
-            }
-        }
 
+        if (cv_bloodBank != null) {
+            cv_bloodBank.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(HomePageView.this, BloodBankView.class));
+                    }
+                });
+            }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_logout:{
+                AlertDialog.Builder  builder = new AlertDialog.Builder(this);
+                builder.setTitle("Alert?");
+                builder.setMessage("Are you sure you want to logout?");
+                builder.setPositiveButton("yes i'm", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        SharedPreferences applicationStatus = HomePageView.this.getSharedPreferences(PREFERENCE_FILE_KEY, Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = applicationStatus.edit();
+                        editor.putBoolean("LOGGED_IN", false);
+                        editor.apply();
+                        startActivity(new Intent(HomePageView.this, LoginView.class));
+                        finish();
+                    }
+                });
+                builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        alertDialog.cancel();
+                    }
+                });
+                alertDialog = builder.create();
+                alertDialog.show();
+                break;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     private boolean isPlayServicesAvailable() {
         int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
         if (resultCode != ConnectionResult.SUCCESS) {
